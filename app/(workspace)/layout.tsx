a9d71a789e2a6c5bfb88ec214a93dbb6def748cb
Surendra Manjhi <manjhisurendra08@gmail.com>
2026-01-29 01:27:52 +0530

    fix: adjust margin in main layout for consistent spacing

--- app/(workspace)/layout.tsx
+++ app/(workspace)/layout.tsx
@@ -9,7 +9,7 @@ export default function WorkspaceLayout({
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="fixed inset-y-0 right-0 left-0 md:left-[calc(var(--sidebar-width))] peer-data-[state=collapsed]:md:left-[calc(var(--sidebar-width-icon)+0.5rem)] transition-[left] duration-200 ease-linear my-2 mr-2">
+      <main className="fixed inset-y-0 right-0 left-0 md:left-[calc(var(--sidebar-width))] peer-data-[state=collapsed]:md:left-[calc(var(--sidebar-width-icon)+0.5rem)] transition-[left] duration-200 ease-linear my-2 mr-2 ml-2 md:ml-0">
         <div className="h-full rounded-lg bg-sidebar shadow-sm ring-1 ring-sidebar-border overflow-auto">
           {children}
         </div>
