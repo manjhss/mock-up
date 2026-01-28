8c29b894ca3b7082f68218fdda33cd046a54eb9a
Surendra Manjhi <manjhisurendra08@gmail.com>
2026-01-29 01:24:51 +0530

    refactor: simplify header component by removing device selection

--- features/preview/components/header.tsx
+++ features/preview/components/header.tsx
@@ -4,50 +4,18 @@ import {
   InputGroupAddon,
   InputGroupInput,
 } from "@/components/ui/input-group";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { Search01Icon } from "@hugeicons/core-free-icons";
 
-const devices = [
-  { label: "Website", value: "website" },
-  { label: "App", value: "app" },
-];
+import { Search01Icon } from "@hugeicons/core-free-icons";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-10 bg-sidebar p-2 flex items-center justify-between border-b border-sidebar-border">
-      <div className="flex-1 flex items-center gap-2">
-        <Select items={devices} defaultValue={"website"}>
-          <SelectTrigger id="device-select" className="w-38">
-            <SelectValue placeholder="Select a device" />
-          </SelectTrigger>
-
-          <SelectContent>
-            <SelectGroup>
-              {devices.map((item) => (
-                <SelectItem key={item.value} value={item.value}>
-                  {item.label}
-                </SelectItem>
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-
-        <InputGroup className="max-w-lg">
-          <InputGroupInput placeholder="Search..." />
-          <InputGroupAddon>
-            <Icon icon={Search01Icon} />
-          </InputGroupAddon>
-        </InputGroup>
-      </div>
-
-      {/* Sponsor me */}
+    <header className="sticky top-0 z-10 bg-sidebar p-2 flex items-center justify-center border-b border-sidebar-border">
+      <InputGroup className="max-w-lg">
+        <InputGroupInput placeholder="Search..." />
+        <InputGroupAddon>
+          <Icon icon={Search01Icon} />
+        </InputGroupAddon>
+      </InputGroup>
     </header>
   );
 }
