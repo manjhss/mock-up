5b651ac7d8e56b2d881a0bc50258f6f7497d68c9
Surendra Manjhi <manjhisurendra08@gmail.com>
2026-02-04 02:07:31 +0530

    refactor: revamp mode-toggle component

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -3,21 +3,40 @@
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
-import { DarkModeIcon } from "@hugeicons/core-free-icons";
-import { HugeiconsIcon } from "@hugeicons/react";
+import {
+  ComputerIcon,
+  Moon02Icon,
+  Sun03Icon,
+} from "@hugeicons/core-free-icons";
+import { ButtonGroup } from "./ui/button-group";
+import Icon from "./icon";
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <Button
-      size={"icon"}
-      variant={"ghost"}
-      onClick={() => {
-        setTheme(theme === "light" ? "dark" : "light");
-      }}
-    >
-      <HugeiconsIcon icon={DarkModeIcon} strokeWidth={2} />
-    </Button>
+    <ButtonGroup className="border rounded-lg">
+      <Button
+        size="icon-sm"
+        variant={theme === "system" ? "secondary" : "ghost"}
+        onClick={() => setTheme("system")}
+      >
+        <Icon icon={ComputerIcon} />
+      </Button>
+      <Button
+        size="icon-sm"
+        variant={theme === "light" ? "secondary" : "ghost"}
+        onClick={() => setTheme("light")}
+      >
+        <Icon icon={Sun03Icon} />
+      </Button>
+      <Button
+        size="icon-sm"
+        variant={theme === "dark" ? "secondary" : "ghost"}
+        onClick={() => setTheme("dark")}
+      >
+        <Icon icon={Moon02Icon} />
+      </Button>
+    </ButtonGroup>
   );
 }
