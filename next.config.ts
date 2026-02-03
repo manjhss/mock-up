0008c5ae9c5e91827645c1d5043c048276d78191
Surendra Manjhi <manjhisurendra08@gmail.com>
2026-02-04 00:48:03 +0530

    feat: configure image remote patterns to allow all HTTPS hostnames

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // Allow all HTTPS hostnames for user-uploaded images
+      },
+    ],
+  },
 };
 
 export default nextConfig;
