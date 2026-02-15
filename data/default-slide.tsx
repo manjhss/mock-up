74f25ebae2b41891c3b2ebbefc8aebce32c51ac4
Surendra Manjhi <manjhisurendra08@gmail.com>
2026-02-15 14:21:59 +0530

    feat: enhance DefaultSlide component

--- data/default-slide.tsx
+++ data/default-slide.tsx
@@ -1,28 +1,48 @@
-import { Style } from "@/zod/schema";
+import { Slide, Style } from "@/zod/schema";
+import Image from "next/image";
 
 interface DefaultSlideProps {
-  data: {
-    logo: string;
-    heading: string;
-    description: string;
-    media: string;
-  };
+  data: Slide["data"];
   style: Style;
 }
 
 export function DefaultSlide({ data, style }: DefaultSlideProps) {
   return (
     <div
-      className="relative h-full aspect-video rounded-md shrink-0 overflow-hidden"
+      className="w-full h-full rounded-md flex flex-col items-center relative overflow-hidden select-none @container-[size]"
       style={{
-        backgroundImage: style.backgroundImage
-          ? `url(${style.backgroundImage})`
-          : undefined,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
+        fontFamily: style.fontFamily,
+        color: style.textColor,
       }}
     >
-      {data.heading}
+      <Image
+        src={style.backgroundImage!}
+        alt="Background"
+        fill
+        className="object-cover object-center"
+        priority
+      />
+
+      <div className="text-center max-w-[85cqw] mt-[4cqw] z-10 shrink-0 space-y-[1cqw]">
+        <h1 className="text-[4.2cqw] font-serif font-medium tracking-tighter leading-[1.05]">
+          {data.heading}
+        </h1>
+
+        <p className="text-[2.3cqw] font-sans opacity-85 font-normal tracking-tight leading-snug">
+          {data.description}
+        </p>
+      </div>
+
+      {/* Mockup Image Container */}
+      <div className="relative w-[90cqw] flex-1 min-h-0 rounded-[2cqw] shadow-[0_4cqw_8cqw_-2cqw_rgba(0,0,0,0.3)] overflow-hidden translate-y-[3cqw]">
+        <Image
+          src={data.media}
+          alt="Mockup Preview"
+          fill
+          className="object-cover object-top"
+          priority
+        />
+      </div>
     </div>
   );
 }
--- data/presets.ts
+++ data/presets.ts
@@ -2,8 +2,9 @@ import { MockUps } from "@/zod/schema";
 import { DefaultSlide } from "@/data/default-slide";
 
 const background = [
-  "https://imgs.search.brave.com/DZ92MZ80cbiZjMKg4-q9xnlCCL2JYSUdRANdPqiMrGw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/MDE2MjQwNzU5NjIt/MDA3ODMzZGYxYWVl/P2ZtPWpwZyZxPTYw/Jnc9MzAwMCZhdXRv/PWZvcm1hdCZmaXQ9/Y3JvcCZpeGxpYj1y/Yi00LjEuMCZpeGlk/PU0zd3hNakEzZkRC/OE1IeHpaV0Z5WTJo/OE1UWjhmR1JsYm5O/bEpUSXdabTl5WlhO/MGZHVnVmREI4ZkRC/OGZId3c",
-  "https://imgs.search.brave.com/VyLhXiU9p4H6ba4k0Uqvf_eXM4Y8s_smuxvvQnzKYOM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTg5/MTIzNzgzMi92ZWN0/b3Ivc29mdC1ncmFp/bnktYmFja2dyb3Vu/ZC13aXRoLW1vdGlv/bi1ibHVyLWFuZC1n/bGl0Y2gtdGVjaG5p/cXVlLmpwZz9zPTYx/Mng2MTImdz0wJms9/MjAmYz1haXRQMnVI/MEVuQVdCal94dFdU/dW00VHFXeUgyaHk3/R0ljci03R19ZZ2tj/PQ",
+  "/green.jpg",
+  "/dark-blue.jpg",
+  "/orange.jpg",
 ];
 
 const textColor = ["#000000", "#FFFFFF"];
