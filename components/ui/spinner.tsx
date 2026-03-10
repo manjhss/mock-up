ea6891876d50de8652093ef565384b18c7a0a559
Surendra Manjhi <manjhisurendra08@gmail.com>
2026-03-10 12:01:46 +0530

    fix: update Spinner component props to omit strokeWidth

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 import { HugeiconsIcon } from "@hugeicons/react"
 import { Loading02Icon } from "@hugeicons/core-free-icons"
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+function Spinner({ className, ...props }: Omit<React.ComponentProps<"svg">, "strokeWidth">) {
   return (
     <HugeiconsIcon icon={Loading02Icon} strokeWidth={2} role="status" aria-label="Loading" className={cn("size-4 animate-spin", className)} {...props} />
   )
